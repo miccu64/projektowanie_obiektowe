f06994c04aa9cbd5398cde34ccf22e4b6a654f88
miccu64 <98konrad98@gmail.com>
2023-06-18 12:43:06 +0200

    z10

--- zadanie10_chmura/front/src/components/Cart.js
+++ zadanie10_chmura/front/src/components/Cart.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
     const navigate = useNavigate();
@@ -25,13 +25,13 @@ function Cart() {
 
     return <div>
         {cart?.map(product => {
-            return <div key={product.id} style={{marginBottom: "10px"}}>
+            return <div key={product.id} style={{ marginBottom: "10px" }}>
                 <h5>Product name: {product.name}</h5>
                 <h5>Quantity: {product.qty}</h5>
                 <h5>Price: {product.price * product.qty}</h5>
             </div>
         })}
-        <form onSubmit={handleSubmit} style={{marginBottom: "10px"}}>
+        <form onSubmit={handleSubmit} style={{ marginBottom: "10px" }}>
             <button type={"submit"}>Pay for all</button>
         </form>
         <button onClick={() => navigate('/')}>Go back</button>
